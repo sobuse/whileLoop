da265f178876db302a20d2b926fec8839c5a5ff9
sammy <ehuerikenbaba@gmail.com>
2021-06-22 15:31:55 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace whileLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var i = 0;
+            //while (i <= 10)
+            //{
+            //    if(i % 2 == 0)
+            //         Console.WriteLine(i);
+
+            //    i++;
+            //}
+
+            //its better to use a while loop when you kwon how many times you are going to do iteration
+
+            while (true)
+            {
+                Console.Write("Type your name: ");
+                var input = Console.ReadLine();
+
+                if (string.IsNullOrWhiteSpace(input))
+                    break;
+                Console.WriteLine( "@Echo: " + input);
+            }
+        }
+    }
+}
